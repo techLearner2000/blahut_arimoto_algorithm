a1909ab905a75783a277e419b30550ca4019ce89
YanMinMacMaster <miny13@mcmaster.ca>
2023-12-10 01:02:34 -0500

    Finish the capacities for awgn.

--- blahut_arimoto_algorithm.py
+++ blahut_arimoto_algorithm.py
@@ -6,7 +6,7 @@
 # The capacity of a channel is defined by <br>
 # $C = \max_{r(x)} I(X;Y) = \max_{r(x)} \sum_{x} \sum_{y} r(x) p(y|x) \log \frac{r(x) p(y|x)}{r(x) \sum_{\tilde{x}} r(\tilde{x})p(y|\tilde{x})}$
 
-# In[13]:
+# In[1]:
 
 
 import numpy as np
@@ -67,7 +67,7 @@ def blahut_arimoto(p_y_x: np.ndarray,  log_base: float = 2, thresh: float = 1e-1
 # The capacity of this channel  <br> 
 # $C = 1 - H_b(P_e)$
 
-# In[14]:
+# In[2]:
 
 
 e = 0.2
@@ -89,7 +89,7 @@ print('Anatliyic capacity: ', (1 - H_P_e))
 # The capacity of this channel is  <br> 
 # $C = 1 - P_e$.
 
-# In[15]:
+# In[3]:
 
 
 e = 0.1
@@ -107,7 +107,7 @@ print('Anatliyic capacity: ', (1 - e))
 # # Converting to executable 
 # Create python file that could be imported to another file.
 
-# In[16]:
+# In[4]:
 
 
 get_ipython().system(' jupyter nbconvert blahut_arimoto_algorithm.ipynb --to="python" --output-dir .')
@@ -115,7 +115,7 @@ get_ipython().system(' jupyter nbconvert blahut_arimoto_algorithm.ipynb --to="py
 
 # # Discrete the alphabet
 
-# In[17]:
+# In[ ]:
 
 
 from scipy.stats import norm
@@ -154,29 +154,97 @@ def discretize_alphabet(A, N, M):
     return x, y, p_y_x
 
 
+# # AWGN
+
+# In[ ]:
+
+
+from matplotlib.pylab import randn
+from numpy import sqrt
+
+
+def awgn(input_signal, snr_dB, rate=1.0):
+    """
+    Addditive White Gaussian Noise (AWGN) Channel.
+
+    Parameters
+    ----------
+    input_signal : 1D ndarray of floats
+        Input signal to the channel.
+
+    snr_dB : float
+        Output SNR required in dB.
+
+    rate : float
+        Rate of the a FEC code used if any, otherwise 1.
+
+    Returns
+    -------
+    output_signal : 1D ndarray of floats
+        Output signal from the channel with the specified SNR.
+    """
+
+    avg_energy = sum(abs(input_signal) * abs(input_signal))/len(input_signal)
+    snr_linear = 10**(snr_dB/10.0)
+    noise_variance = avg_energy/(2*rate*snr_linear)
+
+    if input_signal.dtype == complex:
+        noise = (sqrt(noise_variance) * randn(len(input_signal))) + (sqrt(noise_variance) * randn(len(input_signal))*1j)
+    else:
+        noise = sqrt(2*noise_variance) * randn(len(input_signal))
+
+    output_signal = input_signal + noise
+
+    return output_signal
+
+
+# In[ ]:
+
+
+import matplotlib.pyplot as plt
+
+# Generate a sample signal (e.g., a sinusoidal signal)
+t = np.linspace(0, 1, 1000)  # Time vector
+input_signal = np.sin(2 * np.pi * 5 * t)  # 5 Hz sine wave
+
+# Add AWGN to the signal with a specific SNR
+snr_dB = 10  # Signal-to-noise ratio in dB
+noisy_signal = awgn(input_signal, snr_dB)
+
+# Plot the original and noisy signals
+plt.figure(figsize=(10, 6))
+plt.plot(t, input_signal, label='Original Signal')
+plt.plot(t, noisy_signal, label='Noisy Signal', alpha=0.7)
+plt.title('Original vs Noisy Signal')
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
+plt.legend()
+plt.show()
+
+
 # # Application
 
-# In[20]:
+# In[ ]:
 
 
-# Example usage for different values of A
-A_values = [0.1, 1, 10]
-results = []
+def discretize_input_output(A, num_points=100):
+    X = np.linspace(-A, A, num_points)
+    Y = np.linspace(-A - 3, A + 3, num_points)
+    p_y_x = np.zeros((len(X), len(Y)))
 
-for A in A_values:
-    N = 50  # Number of intervals for the input
-    M = 100  # Number of intervals for the output
+    for i, x in enumerate(X):
+        p_y_x[i, :] = norm.pdf(Y, loc=x, scale=1)
+    p_y_x /= p_y_x.sum(axis=1, keepdims=True)
 
-    # Discretize the input and output alphabets and get the conditional probability matrix
-    x, y, p_y_x = discretize_alphabet(A, N, M)
+    return p_y_x
 
-    # Compute the channel capacity and the optimal input distribution
-    C, r = blahut_arimoto(p_y_x)
-    results.append((A, C, r))
+As = [0.1, 1, 10]
+results = {}
 
-# Display the results
-for result in results:
-    print(f"Peak amplitude constraint (A): {result[0]}")
-    print(f"Computed Channel Capacity: {result[1]}")
-    print(f"Optimal Input Distribution: {result[2]}\n")
+for A in As:
+    p_y_x = discretize_input_output(A)
+    capacity, distribution = blahut_arimoto(p_y_x)
+    results[A] = (capacity, distribution)
+
+print(results)
 
